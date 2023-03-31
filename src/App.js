3991ee6e000f465c26b11da07de3efe7049ba7cf
nakib1948 <hayathossain.jtp@gmail.com>
2023-03-31 19:52:29 +0600

    card part done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useEffect, useState } from 'react';
+import Allblog from './Components/Allblog';
 
 function App() {
+   const [user,setUser]=useState([])
+
+   useEffect(()=>{
+    fetch('data.json')
+    .then(res=>res.json())
+    .then(data=>setUser(data))
+   },[])
+   //console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar className='mt-2'>
+      <Container className='pb-4' style={{borderBottom:'1px solid gray'}}>
+        <Navbar.Brand href="#home" > <h3>Knowledge Cafe</h3></Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <img height='50px' style={{borderRadius:'50%'}} src="https://randomuser.me/api/portraits/men/35.jpg" alt="" />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    <Container className='mt-5'>
+      <Row>
+        <Col sm={8}>
+         {
+            user.map(user=><Allblog data={user}></Allblog>)
+         }
+
+        </Col>
+        <Col sm={4}>sm=4</Col>
+      </Row>
+      
+    </Container>
+    
+    </>
   );
 }
 
---
+++ src/Components/Allblog.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+const Allblog = (props) => {
+    let data=props.data;
+    console.log(data)
+    return (
+        <div className='mb-3'>
+             <Card >
+                <Card.Img variant="top" src={data.picture} />
+                <Card.Body>
+                    <div className='m-3 d-flex justify-content-between align-items-center'>
+                        <div className='d-flex align-items-center'>
+                            <div>
+                                <img height='80px' src={data.image}  style={{borderRadius:'50%'}} alt="" />
+                            </div>
+                            <div style={{marginTop:'10px',marginLeft:'10px'}}>
+                                <h5>{data.name}</h5>
+                                <p>{data.date}</p>
+                            </div>
+                        </div>
+                        <div className='d-flex'>
+                            <p>{data.time} min read</p>
+                            <BookmarkBorderIcon></BookmarkBorderIcon>
+                        </div>
+                        
+                    </div>
+                    <Card.Title>{data.description}</Card.Title>
+                    
+                    <p className='m-2'>#begineer<span style={{marginLeft:'10px'}}>#programming</span></p>
+                    <a href=''>Mark as read</a>
+                </Card.Body>
+             </Card>
+        </div>
+    );
+};
+
+export default Allblog;
