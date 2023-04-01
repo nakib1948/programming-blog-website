d0d0ad6a33039c5fa514d5ae5a451987e6347b69
nakib1948 <hayathossain.jtp@gmail.com>
2023-04-01 10:48:00 +0600

    Blog state vs props and usestate working principle answered

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -6,19 +6,33 @@ import Card from 'react-bootstrap/Card';
 
 const Blog = () => {
     return (
-      <Container fluid>
-        <Row style={{marginLeft:'10%'}}>
+      <Container fluid className='mt-5'>
+        <h1 className='text-center mb-3'>Blog</h1>
+        <Row style={{marginLeft:'10%',marginRight:'10%'}}>
           <Col> 
-          <Card className='w-75'>
+          <Card className='mb-3'>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Props vs state</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
+                Props are used to pass data from a parent component down to its child component. Props are read-only and should not be modified within the child component. A parent component can pass any data to its child component through props, including functions, objects, and primitive values. Props allow for the creation of reusable components that can be easily customized by passing in different props. <br />
+                State, on the other hand, is used to manage data that can change within a component. State is mutable and should only be modified using the setState method provided by React. Changes to state trigger a re-render of the component, updating the UI to reflect the new state. State is local to a component and cannot be accessed or modified by its parent or child components.
                 </Card.Text>
               
             </Card.Body>
             </Card>
+
+            <Card className='mb-3'>
+            <Card.Body>
+                <Card.Title>How does useState work?</Card.Title>
+                <Card.Text>
+                useState is a hook in React that allows functional components to have state. It takes an initial value as its argument and returns an array with the current state value and a function to update it. By calling the update function, the component can change the state, causing a re-render of the component and updating the UI to reflect the new state.
+                </Card.Text>
+              
+            </Card.Body>
+            </Card>
+
+            
+
             </Col>
         </Row>
       </Container>
